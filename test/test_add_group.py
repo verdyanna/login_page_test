efb593412c47ab0ae7132416229b07fa646c7b41
Anna <verdyanna@gmail.com>
2016-04-03 15:45:10 +0300

    03042016

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,12 @@ from model.group import Group
 def test_create_group(app):
     app.group_helper.fill_group_form(Group(name="frtyh", header="dwetreg", footer="wrehgrthb"))
 
+def test_assert_group(app):
+    old_groups = app.group_helper.get_group_list()
+    app.group_helper.fill_group_form(Group(name="frtyh", header="dwetreg", footer="wrehgrthb"))
+    new_groups = app.group_helper.get_group_list()
+    assert len(old_groups)+1 == len(new_groups)
+
 def test_create_mama_group(app):
     app.group_helper.fill_group_form(Group(name="mama", header="Marios", footer="moskva"))
 
