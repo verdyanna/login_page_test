6458af5b9b86e54e70c7f9e2c737180e63ccb737
Anna <verdyanna@gmail.com>
2016-04-04 09:32:06 +0300

    04042016 - сравнение списков при добавлении групп -ок

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,42 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
+
 #def test_create_group(app):
 #   app.group_helper.fill_group_form(Group(name="frtyh", header="dwetreg", footer="wrehgrthb"))
 
-def test_assert_group(app):
+#def test_assert_group(app):
+#   old_groups = app.group_helper.get_group_list()
+ #   app.group_helper.fill_group_form(Group(name="frtyh", header="dwetreg", footer="wrehgrthb"))
+  #  new_groups = app.group_helper.get_group_list()
+   # assert len(old_groups)+1 == len(new_groups)
+
+#def test_create_mama_group(app):
+ #   old_groups = app.group_helper.get_group_list()
+  #  app.group_helper.fill_group_form(Group(name="mama", header="Marios", footer="moskva"))
+   # new_groups = app.group_helper.get_group_list()
+    #assert len(old_groups)+1 == len(new_groups)
+
+#def test_create_empty_group(app):
+#    app.group_helper.fill_group_form(Group(name="", header="", footer=""))
+
+def test_assert_list_group(app):
     old_groups = app.group_helper.get_group_list()
-    app.group_helper.fill_group_form(Group(name="frtyh", header="dwetreg", footer="wrehgrthb"))
+    groupp = Group(name="frtyh", header="dwetreg", footer="wrehgrthb")
+    app.group_helper.fill_group_form(groupp)
     new_groups = app.group_helper.get_group_list()
     assert len(old_groups)+1 == len(new_groups)
+    old_groups.append(groupp)
 
-def test_create_mama_group(app):
+    assert sorted (old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+def test_create_empty_group_assert(app):
     old_groups = app.group_helper.get_group_list()
-    app.group_helper.fill_group_form(Group(name="mama", header="Marios", footer="moskva"))
+    groupp = Group(name="", header="", footer="")
+    app.group_helper.fill_group_form(groupp)
     new_groups = app.group_helper.get_group_list()
     assert len(old_groups)+1 == len(new_groups)
-
-def test_create_empty_group(app):
-    app.group_helper.fill_group_form(Group(name="", header="", footer=""))
+    old_groups.append(groupp)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -8,7 +8,7 @@ def test_delete_first_group(app):
     app.group_helper.delete_first_group()
     new_groups = app.group_helper.get_group_list()
     assert len(old_groups)-1 == len(new_groups)
-    old_groups [0:1] = []
+    old_groups[0:1] = []
     assert old_groups == new_groups
 
 
