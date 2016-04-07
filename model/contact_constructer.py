cb27d8b564d75bd0560388d62293b39a36e277ab
Anna <verdyanna@gmail.com>
2016-04-07 08:12:45 +0300

    5_4 - созданы тесты добавления случайных групп

--- model/contact_constructer.py
+++ model/contact_constructer.py
@@ -2,20 +2,18 @@ from sys import maxsize
 
 class Contact_fill:
 
-    def __init__(self, id=None, firstname=None, middlename=None, lastname=None,
-                 address=None, home=None, mobile=None, work=None, fax=None, phone2=None, email=None):
+    def __init__(self, firstname=None, lastname=None, id=None, home=None, work=None, mobile=None, phone2=None, all_phones_from_home_page = None):
 
         self.id = id
         self.firstname = firstname
-        self.middlename = middlename
         self.lastname = lastname
-        self.address = address
         self.home = home
-        self.mobile = mobile
+
         self.work = work
+        self.mobile = mobile
         self.phone2 = phone2
-        self.fax = fax
-        self.email = email
+        self.all_phones_from_home_page = all_phones_from_home_page
+
 
     def __repr__(self):
         return "%s:%s %s" % (self.id, self.firstname, self.lastname)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,5 +1,34 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols)for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="",footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range (5)
+
+]
+
+    #[
+   #Group(name=name, header=header, footer=footer)
+    #for name in ["", random_string("name", 10)]
+    # for footer in ["", random_string("footer", 20)]]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr (x) for x in testdata])
+def test_add_group_loop(app, group):
+        old_groups = app.group_helper.get_group_list()
+        app.group_helper.fill_group_form(group)
+        assert len(old_groups)+1 == app.group_helper.count()
+        new_groups = app.group_helper.get_group_list()
+        old_groups.append(group)
+        assert sorted (old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 
 #def test_create_group(app):
