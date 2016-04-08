ac929c100c0f4652a04a605c377937bb0846318f
Anna <verdyanna@gmail.com>
2016-04-08 23:44:20 +0300

    6_5 работа с файлами закончена(подгрузка из файла)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,14 +3,25 @@ from model.group import Group
 import pytest
 from data.add_group import constant as testdata
 
-@pytest.mark.parametrize("group", testdata, ids=[repr (x) for x in testdata])
-def test_add_group_loop(app, group):
-        old_groups = app.group_helper.get_group_list()
-        app.group_helper.fill_group_form(group)
-        assert len(old_groups)+1 == app.group_helper.count()
-        new_groups = app.group_helper.get_group_list()
-        old_groups.append(group)
-        assert sorted (old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+def test_add_group_gen(app, json_groups):
+    group = json_groups
+    old_groups = app.group_helper.get_group_list()
+    app.group_helper.fill_group_form(group)
+    assert len(old_groups) + 1 == app.group_helper.count()
+    new_groups = app.group_helper.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+
+#@pytest.mark.parametrize("group", testdata, ids=[repr (x) for x in testdata])
+#def test_add_group_loop(app, group):
+#        old_groups = app.group_helper.get_group_list()
+#        app.group_helper.fill_group_form(group)
+#        assert len(old_groups)+1 == app.group_helper.count()
+ #       new_groups = app.group_helper.get_group_list()
+ #       old_groups.append(group)
+#        assert sorted (old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
